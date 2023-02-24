b5b77dadf1601052310234a192d23edf832d019e
Maicol <michael.negrete16@gmail.com>
2023-02-23 21:57:09 -0500

    Creacion basica del login y registro

--- src/page/loginPage.jsx
+++ src/page/loginPage.jsx
@@ -2,7 +2,42 @@ const LoginPage = () => {
 
     return (
         <div>
-            <h1>Login Page</h1>
+            <div>
+                <h1>Login</h1>
+                <form>
+                    <div>
+                        <input type="email" placeholder="ejemplo@ejemplo.com"/>
+                    </div>
+                    <div>
+                        <input type="password" placeholder="Contraseña"/>
+                    </div>
+                    <div>
+                        <input type="submit" value='Login'/>
+                    </div>
+                </form>
+            </div>
+
+            <div>
+                <h1>Registro</h1>
+                <form>
+                    <div>
+                        <input type="text" placeholder="Nombre" />
+                    </div>
+                    <div>
+                        <input type="email" placeholder="Correo" />
+                    </div>
+                    <div>
+                        <input type="password" placeholder="Contraseña" />
+                    </div>
+                    <div>
+                        <input type="password" placeholder="Repita la contraseña" />
+                    </div>
+                    <div>
+                        <input type='submit' value='Registrar' />
+                    </div>
+                </form>
+            </div>
+
         </div>
     )
 
